8720a856d153201ba294ce66cd0ede2580547a1b
kyleochata <kyleochata@gmail.com>
2023-09-13 18:35:58 -0700

    got most of the code in. issue with the license section

--- assets/utilities/generateMD.js
+++ assets/utilities/generateMD.js
@@ -1,18 +1,77 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+!license ? '':`[![${license} license](https//img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch(license) {
+    case 'MIT':
+    return `https://lbesson.mit-license.org`;
+    break;
+    case 'Apache 2.0':
+      return `https://www.apache.org/licenses/LICENSE-2.0`;
+      break;
+    case 'BSD 3-Clause':
+      return `https://opensource.org/license/bsd-3-clause/`;
+      break;
+    case 'BSD 2-Clause':
+      return `https://opensource.org/license/bsd-2-clause/`;
+      break;
+    case `GNU GPL v3`:
+      return `https://www.gnu.org/licenses/gpl-3.0.en.html`;
+      break;
+    default:
+      return `No license link found`;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  !license ? '': `## License
+  Please see the ${license} license found in the repository. To learn more, please click the license badge at the top of the README.MD`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+
+  return `# ${data.projectTitle}
+${renderLicenseBadge(data.licenseType)}
+
+## Description
+${data.descriptionInput}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Test](#test)
+
+## Installation
+${data.installInput}
+
+
+## Usage
+${data.usageInput}
+
+${renderLicenseSection(data.licenseType)}
+
+## Contributing
+${data.creditInput}
+
+## Test
+${data.testInput}
+
+## Questions
+If you have further questions about this project, please send an email or checkout us out on GitHub.
+Email: ${data.email}
+GitHub Link: https://github.com/${data.githubUsername}
+
+## Credit
 
 `;
 }
--- index.js
+++ index.js
@@ -17,15 +17,90 @@ THEN I am taken to the corresponding section of the README
 */
 
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const genMarkDown = require('./assets/utilities/generateMD')
+const fs = require('fs')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+  type: 'input',
+  message: 'What is your name?',
+  name: 'name',
+},
+{
+  type: 'input',
+  message: 'What is the title of your project?',
+  name: 'projectTitle',
+},
+{
+  type: 'input',
+  message: 'Please add a description of your project.',
+  name: 'descriptionInput',
+},
+{
+  type: 'input',
+  message: 'How would you install this project?',
+  name: 'installInput',
+  default: `Locate a suitable place where you would like to download this repository. Once found, copy the repositories SSH or HTTPS to clone through the terminal or download the zipfile from GitHub. After it is downloaded, right-click the index file and launch with your preferred internet browser. While in the browser, feel free to examine the HTML and CSS code in developer mode by hitting F12 or right-clicking the screen and navigating to inspect.`
+},
+{
+  type: 'input',
+  message: 'How would you like to allow this repository to be used?',
+  name: 'usageInput',
+  default: `This repository is allowed for use in a learning environment to evaluate and analyze.`
+},
+{
+  type: 'input',
+  message: 'Please enter in anyone you wish to give credit to.',
+  name: 'creditInput',
+},
+{
+  type: 'input',
+  message: 'What sequence would you like to initialize the test? Default: npm test',
+  name: 'testInput',
+  default: 'npm test',
+},
+{
+  type: 'list',
+  message: 'What type of license would you like to use? Default: MIT License',
+  choices: [
+    'MIT', new inquirer.Separator(),
+    'Apache 2.0', new inquirer.Separator(),
+    'BSD 3-Clause', new inquirer.Separator(),
+    'BSD 2-Clause', new inquirer.Separator(),
+    'GNU GPL v3', new inquirer.Separator(),
+  ],
+  name: 'licenseType',
+  default: 'MIT License',
+},
+{
+  type: 'input',
+  message: 'What is your GitHub Username?',
+  name: 'githubUsername'
+},
+{type: 'input',
+message: 'What is your email?',
+name: 'email'
+}
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile =  data => {
+fs.writeFile('README.md', data, err => {
+  err ? console.log(err) : console.log(`README file created. Check it out!`)
+})
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+  .prompt(questions)
+  .then(genMarkDown)
+  .then(writeToFile)
+  .catch(err => {
+    console.log(err)
+  })
+}
 
 // Function call to initialize app
 init();
